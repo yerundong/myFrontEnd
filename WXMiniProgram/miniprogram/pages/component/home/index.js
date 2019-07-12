e3d5c7619469bd3935b69ce409952ce3e6a7c1d3
yerundong <414868970@qq.com>
2019-07-12 15:40:48 +0800

    222

---
+++ WXMiniProgram/miniprogram/pages/component/home/index.js
@@ -0,0 +1,36 @@
+const app = getApp();
+
+Page({
+  options: {
+    styleIsolation: 'page-isolated'
+  },
+  data: {
+    msg1: '123',
+    msg2: '456',
+  },
+  /**
+   * @method 监听页面加载
+   */
+  onLoad() {
+  },
+  /**
+     * @method 监听页面显示
+     */
+  onShow() {
+  },
+  /**
+   * @method 监听页面初次渲染完成
+   */
+  onReady() {
+  },
+  /**
+   * @method 监听页面隐藏
+   */
+  onHide() {
+  },
+  /**
+   * @method 生命周期回调—监听页面卸载
+   */
+  onUnload() {
+  },
+})
